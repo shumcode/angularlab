f59b3c4849ad645995e7c972227424f45dfd1894
shumcode <cshumylo@gmail.com>
2017-11-16 16:42:41 -0500

    lmfao

--- Angularlab2/scripts/list-component.js
+++ Angularlab2/scripts/list-component.js
@@ -7,7 +7,7 @@
     controller: "FormController",
     template: `
     
-      <li ng-repeat="task in $ctrl.tasks | filter: $ctrl.taskFilter track by $index" hover-task>{{ task }}<i class="material-icons md-18" ng-click="$ctrl.removeTask()">clear</i></li>
+      <li ng-repeat="task in $ctrl.tasks | filter: $ctrl.taskFilter track by $index" hover-task>{{ task }}<i class="material-icons md-18" ng-click="$ctrl.removeTask($index)" hover-x>clear</i></li>
     
     `
   };
